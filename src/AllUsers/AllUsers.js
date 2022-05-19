c0d5af5efc6aa9ed856d9133c78b92f08fd0c048
Md Antor <md.antor1155@gmail.com>
2022-05-20 04:01:30 +0800

    added delete part of the user

--- src/AllUsers/AllUsers.js
+++ src/AllUsers/AllUsers.js
@@ -2,13 +2,26 @@ import React, { useEffect, useState } from 'react';
 import './AllUsers.css'
 
 const AllUsers = () => {
+    const [deleted, setDeleted] = useState({})
     const [data, setData] = useState([]);
     useEffect(() => {
         fetch('http://localhost:5000/users')
             .then(res => res.json())
             .then(data => setData(data))
-    }, [])
+    }, [, deleted])
     console.log(data);
+
+    const handleDelete = (id) => {
+        const confirm = window.confirm("really want to delete", id);
+        if (confirm) {
+            fetch(`http://localhost:5000/deleteUser/${id}`,{
+                method:'delete'
+            })
+                .then(res => res.json())
+                .then(data => setDeleted(data));
+        }
+       
+    }
 
     return (
         <div className='attendance'>
@@ -23,7 +36,7 @@ const AllUsers = () => {
                     {data.map(res => <tr key={res._id}>
                             <td>{res.name}</td>
                             <td>{res.catagory}</td>
-                            <td><button> <a target='_blank' href="https://console.firebase.google.com/project/authentic-4fa12/authentication/users">delete</a></button></td>
+                            <td><button onClick={()=>{handleDelete(res._id)}}> <a target='_blank' href="https://console.firebase.google.com/project/authentic-4fa12/authentication/users">delete</a></button></td>
                         </tr>)}
                     </tbody>
                 </table>
